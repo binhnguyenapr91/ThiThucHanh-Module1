cfdfff0f13d203429289c793fc42d5b31469215e
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-17 16:20:06 +0700

    fix

--- js/bai3.js
+++ js/bai3.js
@@ -1,24 +1,25 @@
-let input = 4;
+let input = 13;
 
 function isFibonacci(number) {
-    if(number<0){
+    if (number < 0) {
         return false;
-    }else if (number < 4){
+    } else if (number < 4) {
         return true;
-    }
-    let status = false;
-    let fiboNumber1=1;
-    let fiboNumber2=1;
-    let fiboNumber3=2;
+    } else {
+        let status = false;
+        let fiboNumber1 = 1;
+        let fiboNumber2 = 1;
+        let fiboNumber3 = 2;
 
-    while(fiboNumber1+fiboNumber2<=number){
-        fiboNumber3 = fiboNumber1 + fiboNumber2;
-        fiboNumber2 = fiboNumber1;
-        fiboNumber1 = fiboNumber3;
+        while (fiboNumber1 + fiboNumber2 <= number) {
+            fiboNumber3 = fiboNumber1 + fiboNumber2;
+            fiboNumber2 = fiboNumber1;
+            fiboNumber1 = fiboNumber3;
+        }
+        if (number === fiboNumber3) {
+            status = true;
+        }
+        return status;
     }
-    if(number === fiboNumber3){
-        status = true;
-    }
-    return status;
 }
 alert(isFibonacci(input));
